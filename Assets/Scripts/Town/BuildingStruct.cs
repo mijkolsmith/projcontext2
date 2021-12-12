48746de396ab42b26d19dc1f594f47a95677f115
Selawen <37039183+Selawen@users.noreply.github.com>
2021-12-12 14:04:31 +0100

    cleanup and commenting

--- Assets/Scripts/Town/BuildingStruct.cs
+++ Assets/Scripts/Town/BuildingStruct.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Struct to save building info to
+/// </summary>
 [System.Serializable]
     public struct BuildingStruct
 {
